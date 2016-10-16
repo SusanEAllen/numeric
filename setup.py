80fc41f16f5adbece8eaf4b45fa9288d83e8a7fb
Phil Austin <paustin@eos.ubc.ca>
2016-10-16 13:17:33 -0700

    switch to setuptools-git-version

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup
-import versioneer
 
 setup(
     name = "numlabs",
     py_modules=['numlabs'],
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass()
+    version_format='{tag}.dev{commitcount}+{gitsha}',
+    setup_requires=['setuptools-git-version']
 )
 
